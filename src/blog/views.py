a31d78dd5465fca659c7aca4526c323dd42f552e
facetable-lab <diofff6@gmail.com>
2023-05-13 00:33:56 +0300

    Временные контроллеры

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,36 @@
+from django.views import View
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Category, Post
+
 
 def home(request) -> HttpResponse:
-    return HttpResponse('Hello')
+    if request.method == 'GET':
+        return HttpResponse('Hello')
+
+
+class HomeView(View):
+    """Контроллер главной страницы"""
+
+    def get(self, request) -> render:
+        context = {
+            'category_qs': Category.objects.all(),
+            'post_qs': Post.objects.all()
+        }
+
+        return render(request, 'blog/home.html', context)
+
+    def post(self, request) -> HttpResponse:
+        return HttpResponse('POST')
+
+
+class CategoryView(View):
+    """Вывод статей по категории"""
+
+    def get(self, request, category_slug) -> render:
+        context = {
+            'category': Category.objects.get(slug=category_slug)
+        }
+
+        return render(request, 'blog/post_list.html', context)
