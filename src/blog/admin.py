7997a3e27ee33159240aebaf06dc947590886d84
facetable-lab <diofff6@gmail.com>
2023-05-13 00:32:56 +0300

    Регистрация моделей

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Category
+from . import models
 
-admin.site.register(Category)
+admin.site.register(models.Category)
+admin.site.register(models.Tag)
+admin.site.register(models.Post)
+admin.site.register(models.Comment)
