45e67ccb23d36f97b92d6839a033523fa630164f
facetable-lab <diofff6@gmail.com>
2023-05-13 00:34:11 +0300

    Подключил приложение mptt

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mptt',
+
     'blog',
 ]
 
@@ -59,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
