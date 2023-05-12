243e0c4ca8aa9b061899a02ab067fc690af50a8c
facetable-lab <diofff6@gmail.com>
2023-05-13 00:33:36 +0300

    Маршруты

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.HomeView.as_view()),
+    path('category/<slug:category_slug>/', views.CategoryView.as_view(), name='category'),
 ]
